4e0084b78fab606449553ae0343f1f408267126e
Nick Germaine <nickgermaine1024@gmail.com>
2019-04-03 16:11:55 -0300

    init

---
+++ common/models/post-image.js
@@ -0,0 +1,63 @@
+/* eslint-disable camelcase */
+/* eslint-disable max-len */
+'use strict';
+const sharp = require('sharp');
+const fs = require('fs');
+
+const CONTAINER_URL = '/api/containers/';
+module.exports = function(PostImage) {
+  PostImage.upload = function(ctx, options, access_token, post_id, user_id, cb) {
+    if (!options) options = {};
+
+    ctx.req.params.container = 'postImages';
+    if (!fs.existsSync('./server/storage/' + ctx.req.params.container)) {
+      fs.mkdirSync('./server/storage/' + ctx.req.params.container);
+    }
+
+    PostImage.app.models.ImageFile.upload(ctx.req, ctx.result, options, (err, file) => {
+      if (err) {
+        cb(err);
+      } else {
+        var fileInfo = file.files.file[0];
+
+        sharp('./server/storage/' + ctx.req.params.container + '/' + fileInfo.name)
+                .resize(100)
+                .toFile('./server/storage/' + ctx.req.params.container + '/100-' + fileInfo.name, (err) => {
+                  if (!err) {
+                    PostImage.create({
+                      url: CONTAINER_URL + fileInfo.container + '/download/' + fileInfo.name,
+                      thumbnail: CONTAINER_URL + fileInfo.container + '/download/100-' + fileInfo.name,
+                      created_at: new Date(),
+                      postId: post_id,
+                      userId: user_id,
+                    }, (err2, image) => {
+                      if (err2) {
+                        cb(err2);
+                      } else {
+                        cb(null, image);
+                      }
+                    });
+                  }
+                });
+      }
+    });
+  };
+
+  PostImage.remoteMethod(
+        'upload',
+    {
+      description: 'Uploads a file',
+      accepts: [
+                {arg: 'ctx', type: 'object', http: {source: 'context'}},
+                {arg: 'options', type: 'object', http: {source: 'query'}},
+                {arg: 'access_token', type: 'string'},
+                {arg: 'post_id', type: 'string'},
+                {arg: 'user_id', type: 'string'},
+      ],
+      returns: {
+        arg: 'fileObject', type: 'object', root: true,
+      },
+      http: {verb: 'post'},
+    }
+    );
+};
---
+++ server/server.js
@@ -0,0 +1,84 @@
+/* eslint-disable camelcase */
+'use strict';
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+app.models.user.afterRemote('create', (ctx, user, next) => {
+  console.log('New User is', user);
+  app.models.Profile.create({
+    first_name: user.username,
+    created_at: new Date(),
+    userId: user.id,
+  }, (err, result) => {
+    if (!err && result) {
+      console.log('Created new profile!', result);
+    } else {
+      console.log('There is an error.',
+        'This is an error message informing you of an error.',
+        'You do not need to do anything.  Things are fine.', err);
+    }
+    next();
+  });
+});
+
+app.models.Role.find({where: {name: 'admin'}}, (err, role) => {
+  if (!err && role) {
+    console.log('No error, role is', role);
+    if (role.length === 0) {
+      app.models.Role.create({
+        name: 'admin',
+      }, (err2, result) => {
+        if (!err2 && result) {
+          app.models.user.findOne((usererr, user) => {
+            if (!usererr && user) {
+              result.principals.create({
+                principalType: app.models.RoleMapping.USER,
+                principalId: user.id,
+              }, (err3, principal) => {
+                console.log('Created principal', err3, principal);
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+});
+
+app.models.Role.find({where: {name: 'editor'}}, (err, roles) => {
+  if (!err && roles) {
+    if (roles.length === 0) {
+      app.models.Role.create({
+        name: 'editor',
+      }, (creationErr, result) => {
+        console.log(creationErr, result);
+      });
+    }
+  }
+});
