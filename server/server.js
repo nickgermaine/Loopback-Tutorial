9c747bb950c9b593245eec3b47bc0e9f18d81f7d
Nick Germaine <nickgermaine1024@gmail.com>
2019-05-24 02:10:11 -0300

    add port env

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  return app.listen(function() {
+  return app.listen(process.env.PORT || 8080, function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
